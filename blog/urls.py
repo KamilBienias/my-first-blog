7803130b72282c04b6db5b8c36fa5eb107920e7a
KamilBienias <kamil.bienias.math@gmail.com>
2020-01-25 21:29:06 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # Django spodziewa się liczby całkowitej i przekaże jej wartość do widoku jako zmienną pk
 ]
