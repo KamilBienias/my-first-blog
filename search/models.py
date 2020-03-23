a6071e51d8ab2dfd8e4a81b20019b93d3eca3d77
KamilBienias <kamil.bienias.math@gmail.com>
2020-03-23 18:46:31 +0100

    website_address changed to URLField

--- search/models.py
+++ search/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Search(models.Model):
-    website_address = models.CharField(max_length=100)
+    website_address = models.URLField(max_length=1000)
     passed_expression = models.CharField(max_length=100)
