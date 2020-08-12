580044e41eac0ff18369f3c7957b04dd1c7ddec7
KamilBienias <kamil.bienias.math@gmail.com>
2020-08-12 22:16:55 +0200

    __str__ changed

--- todo/models.py
+++ todo/models.py
@@ -13,7 +13,13 @@ class Person(models.Model):
                               default='available')
 
     def __str__(self):
-        return f"Id number: {self.id}, first name: {self.first_name}, last name: {self.last_name}, status: {self.status}"
+        return "Id number: {0}," \
+               " first name: {1}," \
+               " last name: {2}," \
+               " status: {3}".format(self.id,
+                                     self.first_name,
+                                     self.last_name,
+                                     self.status)
 
     def __repr__(self):
         return "<br>"\
@@ -38,49 +44,10 @@ class Task(models.Model):
     description = models.CharField(max_length=200)
     importance = models.DecimalField(max_digits=1, decimal_places=0)
 
-    # def __str__(self):
-    #     return "Task: \"{}\" <br>" \
-    #            "with details: \"{}\" <br>" \
-    #            "has importance: {} (on a scale of 1 to 9) <br>" \
-    #            "is assigned to a person with id: \"{}\" named: {} {}<br><br>".format(
-    #                                                                self.summary,
-    #                                                                self.description,
-    #                                                                self.importance,
-    #                                                                self.person.id,
-    #                                                                self.person.first_name,
-    #                                                                self.person.last_name)
-
     def __str__(self):
         return "Task: \"{}\" " \
                "with details: \"{}\" " \
                "has importance: {} (on a scale of 1 to 9)".format(
-            self.summary,
-            self.description,
-            self.importance)
-
-
-    # def __repr__(self):
-    #     return "<br>"\
-    #            "Id number: {0} <br> " \
-    #            "Task: \"{1}\" <br>" \
-    #            "with details: \"{2}\" <br>" \
-    #            "has importance: {3} (on a scale of 1 to 9) <br>" \
-    #            "is assigned to a person with id: \"{4}\" named: {5} {6}<br><br>".format(
-    #             self.id,
-    #             self.summary,
-    #             self.description,
-    #             self.importance,
-    #             self.person.id,
-    #             self.person.first_name,
-    #             self.person.last_name)
-
-    def __repr__(self):
-        return "<br>" \
-               "Id number: {0} <br> " \
-               "Task: \"{1}\" <br>" \
-               "with details: \"{2}\" <br>" \
-               "has importance: {3} (on a scale of 1 to 9) <br><br>".format(
-            self.id,
             self.summary,
             self.description,
             self.importance)
